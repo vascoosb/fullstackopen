3d2a8e0986be2888cdd75e4c1b84c8c81738759d
Vasco <vascoo.bras@gmail.com>
2024-07-12 20:31:54 +0100

    Finish exercise courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,37 +1,26 @@
-const Header = ({course}) => {
-  return (
-    <h1>{course}</h1>
-  )
-}
+import { useState } from "react"
 
-const Content = ({part, exercise}) => {
-  return (
-    <p>{part} {exercise}</p>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
-const Total = ({total}) => {
-  return (
-    <p>Total of exercises: {total}</p>
-  )
-}
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  
-return (
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+
+  return (
     <div>
-      <Header course = {course}/>
-      <Content part = {part1} exercise = {exercises1} />
-      <Content part = {part2} exercise = {exercises2} />
-      <Content part = {part3} exercise = {exercises3} />
-      <Total total = {exercises1 + exercises2 + exercises3}/>
+      {value}
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
